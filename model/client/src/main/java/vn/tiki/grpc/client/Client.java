7237df9a25df34e40684d3c2556d7c0592e5a4d5
hungbv2 <hungbv2@topica.edu.vn>
2019-05-18 14:27:38 +0700

    Use grpc to create data model

---
+++ model/client/src/main/java/vn/tiki/grpc/client/Client.java
@@ -0,0 +1,45 @@
+package vn.tiki.grpc.client;
+
+//import vn.tiki.grpc.model.product.iphonex.ProductModel.Colors;
+//import vn.tiki.grpc.model.product.iphonex.ProductModel.IphoneX;
+
+public class Client {
+//
+//  IphoneX iphoneX1 = IphoneX.newBuilder()
+//      .setName("Iphone X 64Gb Black")
+//      .setPrice(999)
+//      .setMarketPrice(1099)
+//      .setImagesURL(0, "linkIphoneX1-1")
+//      .setImagesURL(1, "linkIphoneX1-2")
+//      .setImagesURL(2, "linkIphoneX1-3")
+//      .setColor(Colors.BLACK)
+//      .setStorage(64)
+//      .setDescription("Chính hãng, Nguyên seal, Mới 100%, Chưa Active")
+//      .setOrigin("USA")
+//      .setFactoryDatetime("Sat, 18 May 2019 00:28:21 +0700")
+//      .setWeight(174)
+//      .build();
+//
+//  IphoneX iphoneX2 = IphoneX.newBuilder()
+//      .setName("Iphone X 128Gb Yellow")
+//      .setPrice(950)
+//      .setMarketPrice(1050)
+//      .setImagesURL(0, "linkIphoneX2-1")
+//      .setImagesURL(1, "linkIphoneX2-2")
+//      .setImagesURL(2, "linkIphoneX3-3")
+//      .setColor(Colors.YELLOW)
+//      .setStorage(128)
+//      .setDescription("Chính hãng, Nguyên seal, Mới 100%, Chưa Active")
+//      .setOrigin("China")
+//      .setFactoryDatetime("Sat, 18 May 2019 00:28:21 +0700")
+//      .setWeight(174)
+//      .build();
+//
+//  public IphoneX getIphoneX1() {
+//    return iphoneX1;
+//  }
+//
+//  public IphoneX getIphoneX2() {
+//    return iphoneX2;
+//  }
+}
